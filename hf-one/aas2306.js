b5b75e814bea45068ff987c0f9fb8f439a041775
Adriana Selman <aas2306@tc.columbia.edu>
2016-10-18 11:59:20 -0400

    3 Functions Code

--- hf-one/aas2306.js
+++ hf-one/aas2306.js
@@ -1,31 +1,71 @@
-var name = "Adriana";
-var coffeeCups = 1;
-if (coffeeCups < 2) {
-  alert(name + " needs more coffee!");
-}
-else
-{
-    alert("We're good on the coffee front!");
-}
-
-var assignments = 8;
-  if ("assignments" < 3) {
-      alert("Great Job!");
+// getCoffee Function
+function getCoffee(coffeeCups) {
+  if (coffeeCups >= 2) {
+    console.log("You should get some coffee");
   }
-  else
-  {
-      alert("You should probably start working...");
+  else {
+    console.log("You're good for now.");
   }
+}
+getCoffee(1);
 
 
-var dogs = 3;
-console.log("Does Adriana have enough dogs?");
-if (dogs > 7) {
-  console.log("No she does not!");
-  dogs = dogs + 1;
-  console.log("Adriana is getting another dog.");
+// calculateScore Function
+function calculateScore(base, extra) {
+  console.log(base + extra);
 }
-else
-{
-  console.log("Yes she does!");
+console.log(calculateScore(3, 4));
+
+
+// whereShouldIRun Function
+function whereShouldIRun(rain) {
+  if (rain === true) {
+    console.log("You should run inside.");
+  }
+  else {
+    console.log("You should run outside.");
+  }
 }
+whereShouldIRun(true);
+
+
+// function sayHello(name) {
+//   var greeting = "Hello there,";
+//   if (name .length > 4) {
+//   console.log(greeting + " " + name);
+// }
+// }
+//
+// sayHello(Adriana);
+
+// var name = "Adriana";
+// var coffeeCups = 1;
+// if (coffeeCups < 2) {
+//   alert(name + " needs more coffee!");
+// }
+// else
+// {
+//     alert("We're good on the coffee front!");
+// }
+//
+// var assignments = 8;
+//   if ("assignments" < 3) {
+//       alert("Great Job!");
+//   }
+//   else
+//   {
+//       alert("You should probably start working...");
+//   }
+//
+//
+// var dogs = 3;
+// console.log("Does Adriana have enough dogs?");
+// if (dogs > 7) {
+//   console.log("No she does not!");
+//   dogs = dogs + 1;
+//   console.log("Adriana is getting another dog.");
+// }
+// else
+// {
+//   console.log("Yes she does!");
+// }
