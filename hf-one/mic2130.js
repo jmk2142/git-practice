92e2478b3c818b795d6e9448e7dd933c3414090f
Maria Correa <mic2130@tc.columbia.edu>
2016-10-21 21:32:24 -0400

    fixing the functions
    Not working autonomy and seatbelt
    Added math.floor

--- hf-one/mic2130.js
+++ hf-one/mic2130.js
@@ -1,66 +1,77 @@
 
-// 5 Functions for a safe and informed roadtrip
+// 5 FUNCTIONS FOR A SMART SAFETY TRIP
 
-var toll1 = 2;
-var toll2 = 7;
-var toll3 = 4;
+// Variables
+
+var toll1 = 4;
+var toll2 = 6;
+var toll3 = 5;
 var tollTotal = toll1 + toll2 + toll3;
 
 var paved;
 var dirty;
 
-var speed = 120;
-var distance = 500;
-var travelTime = speed/distance;
+var speed = 150;
+var distance = 1000;
+var travelTime = Math.floor(distance/speed);
+var travelTimeMin = (distance/speed*60);
 
-var gasoline = 220;
-var autonomy = gasoline/distance;
+var gasoline = 1000;
+var autonomy = Math.floor(gasoline/distance);
 
-var ClickSeatbelt = Boolean('true');
+var seatbeltClick = true;
 
-
+// Functions
 
 function tollCost(toll1, toll2, toll3){
 if (tollTotal > 0) {
-  return "make sure you bring your wallet, you will need to pay " + tollTotal + "USD in tools";
+  console.log ("make sure you bring your wallet, you will need to pay " + tollTotal + "USD in tolls");
 } else {
-  return "road free of tolls! let's buy a sandwich with the cash!";
+  console.log ("road free of tolls! let's buy a sandwich with your cash!");
 }
 }
 
 function roadType(roadType) {
-if (roadType == "paved"); {
-  return "you will have and easy drive in paved road";
+if (roadType == "paved") {
+  console.log ("you will have and easy drive in paved road");
 } if (roadType == "dirty") {
-  return "be carefull, the route is a dirty road";
+  console.log ("be carefull, the route is a dirty road");
 }
 }
 
-function calculeTravelTime(speed, distance){
-return "you will arrive in aprox " + travelTime + " hr to your destiny";
+function getTravelTime(speed, distance){
+ if (travelTime === 0) {
+console.log ("you will arrive in aprox " + travelTimeMin + " min to your destiny");
+  if (150 > travelTime >= 1){
+console.log ("you will arrive in aprox " + travelTime + " hr to your destiny");
 } if (speed >= 150) {
-return "you are going to fast, calm down";
+console.log ("you are going to fast, calm down");
+}
+}
 }
 
 function carAutonomy(gasoline, distance){
   if (gasoline > 200) {
-  return "your gasoline tank is full; you can drive "+ autonomy + " km until you need to refill";
+  console.log ("you can drive "+ autonomy + " km until you need to refill");
 } if (gasoline < 10) {
-  return "your tank is almost empty, find a service station in the next"+ autonomy + " before you start your trip";
+  console.log ("your tank is almost empty, find a service station in the next"+ autonomy + " before you start your trip");
 }
 }
 
-function seatbeltCheck(ClickSeatbelt) {
-if (ClickSeatbelt = 'true') {
+function seatbeltCheck(seatbeltClick) {
+if (seatbeltClick === true) {
   console.log ("wau, that seatbelt looks great on you! safety is trendy");
 } else {
   console.log ("make sure you put on your seatbelt before you start your trip");
 }
 }
 
+// Calling the functions.
 
-console.log (tollCost(2,3,4));
-roadType("dirty");
-calculeTravelTime(180, 60);
-carAutonomy(300, 60);
-seatbeltChek(true);
+console.log (tollCost(2, 7, 4));
+console.log (roadType("dirty"));
+console.log (getTravelTime(180, 60));
+console.log (carAutonomy(300, 60));
+console.log (seatbeltChek(true));
+
+// It is possible to see that 3 functions are working and 2 no, for example the boolean seatbeltChek function is not working and neither the carAutonomy one.  I'm trying hard to figure out why. I guess I'm learning.
